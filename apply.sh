30ee481029166d8bc6a5283b76cb88244e6cc03e
Younes <younes@interactive-object.com>
2011-11-07 14:41:09 +0100

    add apply and create scripts

---
+++ apply.sh
@@ -0,0 +1,14 @@
+cd sources\ejabberd_2.1.x
+patch -p1 -i ../ejabberd_check.erl.patch
+patch -p1 -i ../ejabberd_app.erl.patch
+patch -p1 -i ../mod_privacy_odbc.erl.patch
+patch -p1 -i ../mod_pubsub.erl.patch
+patch -p1 -i ../mod_pubsub_odbc.erl.patch
+patch -p1 -i ../node_hometree_odbc.erl.patch
+patch -p1 -i ../node_pep_odbc.erl.patch
+patch -p1 -i ../nodetree_tree_odbc.erl.patch
+patch -p1 -i ../ejabberd_odbc.erl.patch
+patch -p1 -i ../odbc_queries.erl.patch
+
+
+
---
+++ create.sh
@@ -0,0 +1,18 @@
+diff -crB ejabberd-origin/src/ejabberd_check.erl ejabberd/src/ejabberd_check.erl > ejabberd_check.erl.patch
+diff -crB ejabberd-origin/src/ejabberd_app.erl ejabberd/src/ejabberd_app.erl > ejabberd_app.erl.patch
+diff -crB ejabberd-origin/src/mod_privacy_odbc.erl ejabberd/src/mod_privacy_odbc.erl > mod_privacy_odbc.erl.patch
+
+
+diff -crB ejabberd-origin/src/mod_pubsub/mod_pubsub.erl ejabberd/src/mod_pubsub/mod_pubsub.erl > mod_pubsub.erl.patch
+diff -crB ejabberd-origin/src/mod_pubsub/mod_pubsub_odbc.erl ejabberd/src/mod_pubsub/mod_pubsub_odbc.erl > mod_pubsub_odbc.erl.patch
+diff -crB ejabberd-origin/src/mod_pubsub/node_hometree_odbc.erl ejabberd/src/mod_pubsub/node_hometree_odbc.erl > node_hometree_odbc.erl.patch
+
+
+diff -crB ejabberd-origin/src/mod_pubsub/node_pep_odbc.erl ejabberd/src/mod_pubsub/node_pep_odbc.erl > node_pep_odbc.erl.patch
+diff -crB ejabberd-origin/src/mod_pubsub/nodetree_tree_odbc.erl ejabberd/src/mod_pubsub/nodetree_tree_odbc.erl >nodetree_tree_odbc.erl.patch
+
+
+diff -crB ejabberd-origin/src/odbc/ejabberd_odbc.erl ejabberd/src/odbc/ejabberd_odbc.erl > ejabberd_odbc.erl.patch
+
+diff -crB ejabberd-origin/src/odbc/odbc_queries.erlejabberd/src/odbc/odbc_queries.erl > odbc_queries.erl.patch
+
